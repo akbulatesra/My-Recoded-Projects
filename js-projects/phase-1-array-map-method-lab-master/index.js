0eb60bd9acb6fd889de3cbc3381f1f8843ca00a6
esra akbulat <esra@klinikya.com>
2022-12-14 20:14:28 +0300

    Completed assignment

---
+++ js-projects/phase-1-array-map-method-lab-master/index.js
@@ -0,0 +1,23 @@
+const tutorials = [
+  'what does the this keyword mean?',
+  'What is the Constructor OO pattern?',
+  'implementing Blockchain Web API',
+  'The Test Driven Development Workflow',
+  'What is NaN and how Can we Check for it',
+  'What is the difference between stopPropagation and preventDefault?',
+  'Immutable State and Pure Functions',
+  'what is the difference between == and ===?',
+  'what is the difference between event capturing and bubbling?',
+  'what is JSONP?',
+];
+
+const titleCased = () => {
+  return tutorials.map((item) => {
+    const words = item.split(' ');
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+    }
+    const result = words.join(' ');
+    return result;
+  });
+};
