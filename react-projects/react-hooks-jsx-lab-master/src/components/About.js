0f95c5167957235b98b42c428274ae4e3515eb61
esra akbulat <esra@klinikya.com>
2022-12-15 23:26:37 +0300

    Completed assignment

---
+++ react-projects/react-hooks-jsx-lab-master/src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { image } from '../data/data';
+
+function About() {
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>esra</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
+}
+
+export default About;
