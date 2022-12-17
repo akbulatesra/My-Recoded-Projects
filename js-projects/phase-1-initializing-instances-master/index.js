b7bd6b6870e2c9c2a1e78784376209f7c832a992
esra akbulat <esra@klinikya.com>
2022-12-17 22:58:49 +0300

    Completed assignment

---
+++ js-projects/phase-1-initializing-instances-master/index.js
@@ -0,0 +1,23 @@
+// Write your code here
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.drink = drink;
+    this.soup = soup;
+  }
+}
+class Dinner {
+  #dessert;
+  constructor(salad, soup, entree, dessert) {
+    this.salad = salad;
+    this.#dessert = dessert;
+    this.soup = soup;
+    this.entree = entree;
+  }
+}
