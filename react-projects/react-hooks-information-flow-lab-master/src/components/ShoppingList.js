05a0ab53f81215d3380e73f84e270b8ad4edd103
esra akbulat <esra@klinikya.com>
2022-12-17 23:18:21 +0300

    Completed assignment

---
+++ react-projects/react-hooks-information-flow-lab-master/src/components/ShoppingList.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import Item from './Item';
+import Filter from './Filter';
+
+function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState('All');
+
+  function onCategoryChange(event) {
+    setSelectedCategory(event.target.value);
+  }
+
+  const itemsToDisplay = items.filter((item) => {
+    if (selectedCategory === 'All') return true;
+
+    return item.category === selectedCategory;
+  });
+
+  return (
+    <div className="ShoppingList">
+      <Filter onCategoryChange={onCategoryChange} />
+      <ul className="Items">
+        {itemsToDisplay.map((item) => (
+          <Item key={item.id} name={item.name} category={item.category} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ShoppingList;
