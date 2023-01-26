23aeb63833fcee0488797a60fb863775777e95d0
esra akbulat <esra@klinikya.com>
2023-01-26 19:26:25 +0300

    Completed assignment

---
+++ js-projects/phase-0-pac-3-arithmetic-lab-master/index.js
@@ -0,0 +1,32 @@
+const makeInt = (n) => {
+  const parsedInt = parseInt(n, 10);
+  return parsedInt;
+};
+const preserveDecimal = (n) => {
+  const parsedFloat = parseFloat(n);
+  return parsedFloat;
+};
+const add = (a, b) => {
+  const result = a + b;
+  return result;
+};
+const subtract = (a, b) => {
+  const result = a - b;
+  return result;
+};
+const multiply = (a, b) => {
+  const result = a * b;
+  return result;
+};
+const divide = (a, b) => {
+  const result = a / b;
+  return result;
+};
+const increment = (n) => {
+  const result = n + 1;
+  return result;
+};
+const decrement = (n) => {
+  const result = n - 1;
+  return result;
+};
